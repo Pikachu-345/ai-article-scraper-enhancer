e3cc4b771e5be1d6d421a87ed2fe8fa9439ae2e9
Pikachu <vishal2patel4@gmail.com>
2025-12-30 00:14:52 +0530

    CORS fix

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
